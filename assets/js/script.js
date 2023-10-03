0fd5ce6283fa99f6fe680ad0f357c280c48e1533
Nemanja <nemanja7popovic@gmail.com>
2023-10-03 02:19:35 +0200

    slider code and navbar code fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -81,34 +81,76 @@ var lightBox = function (imgClass) {
     }
 }
 
-var navbarSetup = () => {
-    var navbarDropdown = document.getElementById("navbarDropdown");
-    var dropdownmenu = document.getElementsByClassName("dropdown-menu");
-    var navbartoggler = document.getElementsByClassName("navbar-toggler");
-    var navbarcollapse = document.getElementsByClassName("navbar-collapse");
+/*
+* USAGE: new bootstrapCarousel('carouselID').init();
+* */
+var bootstrapCarousel = function(customID) {
+    this.carousel = document.getElementById(customID);
+    this.slides = this.carousel.querySelectorAll('.carousel-item');
+    this.indicators = this.carousel.querySelectorAll('.carousel-indicators button');
+    this.currentSlide = 0;
 
-    window.addEventListener("click",(e)=>{
-        if(!e.target.closest(".navbar-toggler") && !e.target.closest(".collapse")){
-            navbarcollapse[0].classList.remove("show");
-            navbartoggler[0].classList.add("collapsed");
-        }
-    });
+    this.showSlide = (slideIndex) => {
+        this.slides[this.currentSlide].classList.remove('active');
+        this.slides[slideIndex].classList.add('active');
+        this.indicators[this.currentSlide].classList.remove('active');
+        this.indicators[slideIndex].classList.add('active');
+        this.currentSlide = slideIndex;
+    };
 
-    if(navbartoggler.length>0 && navbarcollapse.length>0){
-        ['click'].forEach((event)=>{
-            navbartoggler[0].addEventListener(event,(e)=>{
-                console.log("click");
-                if(e.target.classList.contains("collapsed")){
-                    navbarcollapse[0].classList.add("show");
-                    e.target.classList.remove("collapsed");
-                }else{
-                    navbarcollapse[0].classList.remove("show");
-                    e.target.classList.add("collapsed");
+    this.init = () => {
+        this.carousel.querySelector('.carousel-control-prev').addEventListener('click', () => {
+            const prevSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+            this.showSlide(prevSlide);
+        });
+
+        this.carousel.querySelector('.carousel-control-next').addEventListener('click', () => {
+            const nextSlide = (this.currentSlide + 1) % this.slides.length;
+            this.showSlide(nextSlide);
+        });
+
+        this.indicators.forEach((indicator, index) => {
+            indicator.addEventListener('click', () => {
+                this.showSlide(index);
+            });
+        });
+    }
+}
+
+/*
+* USAGE: new bootstrapNavbar().init();
+* */
+var bootstrapNavbar = function () {
+    this.navbarTogglerButton = document.getElementsByClassName('navbar-toggler')[0];
+    this.navbarCollapse = document.getElementById('navbarSupportedContent');
+    this.dropdowns = document.querySelectorAll('.nav-item.dropdown');
+
+    this.init = () => {
+        this.navbarTogglerButton.addEventListener('click', () => {
+            this.navbarCollapse.classList.toggle('show');
+        });
+
+        this.dropdowns.forEach(dropdown => {
+            const dropdownToggle = dropdown.querySelector('.nav-link.dropdown-toggle');
+            const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+
+            dropdownToggle.addEventListener('click', (e) => {
+                e.preventDefault();
+                dropdownMenu.classList.toggle('show');
+            });
+        });
+
+        document.addEventListener('click', (e) => {
+            this.dropdowns.forEach(dropdown => {
+                const dropdownToggle = dropdown.querySelector('.nav-link.dropdown-toggle');
+                const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+
+                if (!dropdown.contains(e.target)) {
+                    dropdownMenu.classList.remove('show');
                 }
             });
         });
     }
 }
 
-
-navbarSetup();
+new bootstrapNavbar().init();
